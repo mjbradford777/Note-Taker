0a56cb4a7a744431f8c1cc00fa03a3239875b17c
Red Skull <bradfordmichael7@gmail.com>
2020-02-03 18:44:08 -0500

    added paths for home and notes html pages

---
+++ public/assets/js/server.js
@@ -0,0 +1,14 @@
+let PORT = process.env.PORT || 3000;
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// require("./apiRoutes")(app);
+require("./htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+  });
