03cbdb9fad90539e6479a7a3e8ceaa7ba7ad6dcc
Red Skull <bradfordmichael7@gmail.com>
2020-02-08 12:38:56 -0500

    added delete method, pushing to troubleshoot

--- public/assets/js/apiRoutes.js
+++ public/assets/js/apiRoutes.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const fs = require('fs');
 const noteData = require('../../../db/db.json');
-let location;
 
 module.exports = function(app) {  
     app.get("/api/notes", function(req, res) {
@@ -10,9 +9,28 @@ module.exports = function(app) {
 
     app.post("/api/notes", function(req, res) {
       console.log(req.body);
+      console.log(noteData.length);
+      req.body.id = noteData.length;
       noteData.push(req.body);
       console.log(noteData);
-      fs.writeFile('db.json', JSON.stringify(noteData), (err) => {
+      fs.writeFile('../../../db/db.json', JSON.stringify(noteData), (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+      });
+      res.json(noteData);
+    });
+
+    app.delete(`/api/notes/:id`, function(req, res) {
+      console.log(req.body);
+      noteData.splice(req.body.id, 1);
+      fs.writeFile('../../../db/db.json', JSON.stringify(noteData), (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+      });
+      for (let i = 0; i < noteData.length; i++) {
+        nodeData[i].id = i;
+      }
+      fs.writeFile('../../../db/db.json', JSON.stringify(noteData), (err) => {
         if (err) throw err;
         console.log('The "data to append" was appended to file!');
       });
